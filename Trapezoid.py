d15401023513093acdee36029681fb0defa3fa97
krissylicious <127299518+krissylicious@users.noreply.github.com>
2023-03-25 21:43:10 +0800

    Add files via upload

---
+++ Trapezoid.py
@@ -0,0 +1,6 @@
+
+a= int(input("Enter the short base: "))
+b= int(input("Enter the long base: "))
+height=int(input("Enter the height: "))
+area = ((a + b) / 2 ) * height
+print("The area of a trapezoid is", (int(area)))
---
+++ user.py
@@ -0,0 +1,21 @@
+print("Create your own username and password")
+userName=str(input("Enter your desired Username: "))
+password=str(input("Enter your desired password: "))
+print("Desired Username and Password were Created!")
+
+print("Now try to login your username and password!")
+user_name=str(input("Enter your Username: "))
+passWord=str(input("enter your Password: "))
+
+
+if userName == user_name and password == passWord:
+    print("Welcome! You made it")
+else:
+    print("Error! username and password")
+
+
+    
+
+    
+
+ 
